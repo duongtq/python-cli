62ce409105c28f4bb73e05cb7bc8a146351bf84e
Khanh Duong <team@example.com>
2019-08-28 16:24:10 +0700

    Complete CLI

--- cli.py
+++ cli.py
@@ -1,33 +1,32 @@
-import requests
 import sys
+import requests as rq
 
-LIST_ACTION = "LIST"
-ADD_ACTION = "ADD"
-REMOVE_ACTION = "REMOVE"
+LIST_ACTION     = "LIST"
+ADD_ACTION      = "ADD"
+REMOVE_ACTION   = "REMOVE"
+actions         = [LIST_ACTION, ADD_ACTION, REMOVE_ACTION]
 
-actions = [LIST_ACTION, ADD_ACTION, REMOVE_ACTION]
 ADDRESS_MAPPING = "ADDRESSMAPPINGS"
+REGEX_MAPPING   = "REGEXMAPPINGS"
+USER_MAPPING    = "USERMAPPINGS"
+mappings        = [ADDRESS_MAPPING, REGEX_MAPPING, USER_MAPPING]
 
-action = sys.argv[1]
-mapping = sys.argv[2]
-host = sys.argv[3]
-port = sys.argv[4]
+host            = sys.argv[1]
+port            = sys.argv[2]
+action          = sys.argv[3]
+mapping         = sys.argv[4] 
+# source          = sys.argv[5]
+# destination     = sys.argv[6]
 
-if action == LIST_ACTION and mapping == ADDRESS_MAPPING:
-    # do something
-    # get path and use requests lib to send to JAMES
-    print("1")
+PATH            = 'http://' + host + ':' + port
 
-if action == ADD_ACTION and mapping == ADDRESS_MAPPING:
-    # do something
-    print("2")
+# if action == ADD_ACTION:
+#     if mapping == ADDRESS_MAPPING:
+#         rq.put(PATH + "/domains/domain.tld")
+#         response = rq.post(PATH + "/mappings/address/source@domain.tld/targets/alice@domain.tld")
+#         print(response.status_code)
 
-if action == REMOVE_ACTION and mapping == ADDRESS_MAPPING:
-    # do something
-    print("3")
-
-if action not in actions:
-    if mapping == ADDRESS_MAPPING:
-        print("Invalid request action on %.") % (ADDRESS_MAPPING)
-    else:
-        print("Invalid request action and mapping.")
+if action == LIST_ACTION:
+    if mapping == USER_MAPPING:
+        response = rq.get(PATH + "/mappings")
+        print(response.status_code)
